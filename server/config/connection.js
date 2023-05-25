55ce2c0ba9651af587098e6b4e50b934cf2d34e6
gabetuason <119137013+gabetuason@users.noreply.github.com>
2023-05-25 12:00:38 -0400

    Updated MongoDB connection~

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 module.exports = mongoose.connection;
