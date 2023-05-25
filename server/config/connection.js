981d6d504eb4d41a45a0169eb62646ec264780b9
gabetuason <119137013+gabetuason@users.noreply.github.com>
2023-05-25 13:14:38 -0400

    reverted changes regarding connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
