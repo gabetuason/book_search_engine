cc07434718a0618f3bca747c3bc4c96f3f5e8254
gabetuason <119137013+gabetuason@users.noreply.github.com>
2023-05-25 20:00:05 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dataBase24:UEMjz1z7tnyZ4zLq@cluster0.8gkvzvy.mongodb.net/googlebooks?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
